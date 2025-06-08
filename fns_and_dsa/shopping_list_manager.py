29bb9543c900113bf0f9691a67a4f5a8d5b9c6f2
Mitchel Nwachukwu <nwachukwumitchel53@gmail.com>
2025-06-08 09:07:29 -0700

    added functionality to interactively manage a shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
 shopping_list = []
 while True:
-    print("What would you like to do?")
+    print("Shopping List Manager")
     print("1. Add item")
     print("2. Remove item")
     print("3. View list")
