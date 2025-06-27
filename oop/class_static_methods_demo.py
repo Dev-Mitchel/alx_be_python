719ea471b174374e162a533b5ede31792310e43e
Mitchel Nwachukwu <nwachukwumitchel53@gmail.com>
2025-06-27 15:15:03 -0700

    Distinguishing between static methods and classic methods

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,17 @@
+class Calculator:
+    # class attribute
+    calculator_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+     
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculator type : {cls.calculator_type}")
+        return a * b
+    
+       
+        
+  
+        
